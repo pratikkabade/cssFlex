9bd0c20d7737fa281b12590c769be04b4bfbfdff
Pratik Kabade <76637730+pratikkabade@users.noreply.github.com>
2022-05-16 12:36:32 +0530

    Changes in position properties

--- src/app.js
+++ src/app.js
@@ -14,42 +14,197 @@ function cdb() {
 }
 
 // justify content 
+// flex start 
 function jcfs(){
   document.getElementById('screen').style.justifyContent = 'flex-start';
 }
 function cjcfs() {
   navigator.clipboard.writeText("justify-content: flex-start;");
 }
-
+// flex end 
 function jcfe(){
   document.getElementById('screen').style.justifyContent = 'flex-end';
 }
 function cjcfe() {
   navigator.clipboard.writeText("justify-content: flex-end;");
 }
-
+// center 
 function jcc(){
   document.getElementById('screen').style.justifyContent = 'center';
 }
 function cjcc() {
   navigator.clipboard.writeText("justify-content: center;");
 }
-
+// space evenly 
 function jcse(){
   document.getElementById('screen').style.justifyContent = 'space-evenly';
 }
 function cjcse() {
   navigator.clipboard.writeText("justify-content: space-evenly;");
 }
-
+// space between
 function jcsb(){
   document.getElementById('screen').style.justifyContent = 'space-between';
 }
 function cjcsb() {
   navigator.clipboard.writeText("justify-content: space-between;");
 }
+// space around 
+function jcsa(){
+  document.getElementById('screen').style.justifyContent = 'space-around';
+}
+function cjcsa() {
+  navigator.clipboard.writeText("justify-content: space-between;");
+}
 
+// align-items 
+// flex start 
+function aifs(){
+  document.getElementById('screen').style.alignItems = 'flex-start';
+}
+function caifs() {
+  navigator.clipboard.writeText("align-items: flex-start;");
+}
+// flex end 
+function aife(){
+  document.getElementById('screen').style.alignItems = 'flex-end';
+}
+function caife() {
+  navigator.clipboard.writeText("align-items: flex-end;");
+}
+// stretch 
+function aifst(){
+  document.getElementById('screen').style.alignItems = 'stretch';
+}
+function caifst() {
+  navigator.clipboard.writeText("align-items: stretch;");
+}
+// center 
+function aic(){
+  document.getElementById('screen').style.alignItems = 'center';
+}
+function caic() {
+  navigator.clipboard.writeText("align-items: center;");
+}
 
+// align-content 
+// flex start 
+function acfs(){
+  document.getElementById('screen').style.alignContent = 'flex-start';
+}
+function cacfs() {
+  navigator.clipboard.writeText("align-content: flex-start;");
+}
+// flex end 
+function acfe(){
+  document.getElementById('screen').style.alignContent = 'flex-end';
+}
+function cacfe() {
+  navigator.clipboard.writeText("align-content: flex-end;");
+}
+// stretch 
+function acsb(){
+  document.getElementById('screen').style.alignContent = 'space-between';
+}
+function cacsb() {
+  navigator.clipboard.writeText("align-content: space-between;");
+}
+// center 
+function acc(){
+  document.getElementById('screen').style.alignContent = 'center';
+}
+function cacc() {
+  navigator.clipboard.writeText("align-content: center;");
+}
+
+// hide element 
+var a = document.getElementById("toggleSwitch1");
+var b = document.getElementById("toggleSwitch2");
+var c = document.getElementById("toggleSwitch3");
+var d = document.getElementById("toggleSwitch4");
+
+function hide1() {
+  if (a.style.display === "none") {
+    document.getElementById('a').style.boxShadow = '0 1px 10px rgba(139, 181, 221, 0.781)';
+    document.getElementById('aa').style.fontWeight = '550';
+    document.getElementById('b').style.boxShadow = 'none';
+    document.getElementById('bb').style.fontWeight = '400';
+    document.getElementById('c').style.boxShadow = 'none';
+    document.getElementById('cc').style.fontWeight = '400';
+    document.getElementById('d').style.boxShadow = 'none';
+    document.getElementById('dd').style.fontWeight = '400';
+    a.style.display = "block";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";    
+  } else {
+    document.getElementById('a').style.boxShadow = 'none';
+    document.getElementById('aa').style.fontWeight = '400';
+    a.style.display = "none";
+    
+  }
+}
+function hide2() {
+  if (b.style.display === "none") {
+    document.getElementById('b').style.boxShadow = '0 1px 10px rgba(139, 181, 221, 0.781)';
+    document.getElementById('bb').style.fontWeight = '550';
+    document.getElementById('a').style.boxShadow = 'none';
+    document.getElementById('aa').style.fontWeight = '400';
+    document.getElementById('c').style.boxShadow = 'none';
+    document.getElementById('cc').style.fontWeight = '400';
+    document.getElementById('d').style.boxShadow = 'none';
+    document.getElementById('dd').style.fontWeight = '400';
+
+    b.style.display = "block";
+    a.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+  } else {
+    document.getElementById('b').style.boxShadow = 'none';
+    document.getElementById('bb').style.fontWeight = '400';
+    b.style.display = "none";
+  }
+}
+function hide3() {
+  if (c.style.display === "none") {
+    document.getElementById('c').style.boxShadow = '0 1px 10px rgba(139, 181, 221, 0.781)';
+    document.getElementById('cc').style.fontWeight = '550';
+    document.getElementById('a').style.boxShadow = 'none';
+    document.getElementById('aa').style.fontWeight = '400';
+    document.getElementById('b').style.boxShadow = 'none';
+    document.getElementById('bb').style.fontWeight = '400';
+    document.getElementById('d').style.boxShadow = 'none';
+    document.getElementById('dd').style.fontWeight = '400';
+    c.style.display = "block";
+    a.style.display = "none";
+    b.style.display = "none";
+    d.style.display = "none";
+  } else {
+    document.getElementById('c').style.boxShadow = 'none';
+    document.getElementById('cc').style.fontWeight = '400';
+    c.style.display = "none";
+  }
+}
+function hide4() {
+  if (d.style.display === "none") {
+    document.getElementById('d').style.boxShadow = '0 1px 10px rgba(139, 181, 221, 0.781)';
+    document.getElementById('dd').style.fontWeight = '550';
+    document.getElementById('a').style.boxShadow = 'none';
+    document.getElementById('aa').style.fontWeight = '400';
+    document.getElementById('b').style.boxShadow = 'none';
+    document.getElementById('bb').style.fontWeight = '400';
+    document.getElementById('c').style.boxShadow = 'none';
+    document.getElementById('cc').style.fontWeight = '400';
+    d.style.display = "block";
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+  } else {
+    document.getElementById('d').style.boxShadow = 'none';
+    document.getElementById('dd').style.fontWeight = '400';
+    d.style.display = "none";
+  }
+}
 // reload 
 function reload(){
     location.reload();
